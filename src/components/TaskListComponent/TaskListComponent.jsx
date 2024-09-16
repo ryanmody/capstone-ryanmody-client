690a96b89ba891b67434adbf3049ebfdde996eb2
ryanmody <rahimmody488@gmail.com>
2024-09-16 19:55:11 -0400

    Create the component files for TaskList

---
+++ src/components/TaskListComponent/TaskListComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './TaskListComponent.scss'
+
+const TaskListComponent = () => {
+  return (
+    <>
+        <div className='task-list'>
+            <p>Today (Jan 1st):</p>
+
+        </div>
+    
+    </>
+  )
+}
+
+export default TaskListComponent
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SubheaderComponent from '../../components/SubheaderComponent/SubheaderComponent'
 import CalendarComponent from '../../components/CalendarComponent/CalendarComponent'
+import TaskListComponent from '../../components/TaskListComponent/TaskListComponent'
 
 const HomePage = () => {
   return (
     <>
         <SubheaderComponent/>
         <CalendarComponent />
+        <TaskListComponent />
     </>
   )
 }
