5fbdd9e14de52b716dfc94b95e473eca2f355a1d
ryanmody <rahimmody488@gmail.com>
2024-09-16 19:43:38 -0400

    Add calendar component and apply relevant stylings. Update variables stylesheet to include new colors

--- src/components/CalendarComponent/CalendarComponent.jsx
+++ src/components/CalendarComponent/CalendarComponent.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './CalendarComponent.scss'
 
 const CalendarHeaderComponent = () => {
+
+  const dates = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+
   return (
     <>
         <div className='daysofweek'>
@@ -12,6 +15,13 @@ const CalendarHeaderComponent = () => {
           <p className='daysofweek__day'>Thu</p>
           <p className='daysofweek__day'>Frid</p>
           <p className='daysofweek__day'>Sat</p>
+        </div>
+
+        <div className='calendar'>
+
+          {dates.map(date => (
+            <div className='calendar__date'>{date}</div>
+          ))}
         </div>
     </>
   )
