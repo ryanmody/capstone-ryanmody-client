5ac620641eb36b03ae525aa7ee7f2c17e0be20f1
ryanmody <rahimmody488@gmail.com>
2024-09-16 20:15:57 -0400

    Add the task list area for the homepage. For now, only mock-up info is used. Will need to add the logic for generating content dynamically later

--- src/components/TaskListComponent/TaskListComponent.jsx
+++ src/components/TaskListComponent/TaskListComponent.jsx
@@ -4,9 +4,11 @@ import './TaskListComponent.scss'
 const TaskListComponent = () => {
   return (
     <>
-        <div className='task-list'>
-            <p>Today (Jan 1st):</p>
 
+        <h1 className='tasks-title'>Tasks for Jan 1st:</h1>
+
+        <div className='tasks-list'>
+            <div className='tasks-list__category'>Networking, Career, and Events</div>
         </div>
     
     </>
