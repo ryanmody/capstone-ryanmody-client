fa513db5a457f63794bcba7be9d4540d137b0320
ryanmody <rahimmody488@gmail.com>
2024-09-17 17:16:59 -0400

    Have categories generate dynamically from axios call

--- src/components/TaskListComponent/TaskListComponent.jsx
+++ src/components/TaskListComponent/TaskListComponent.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './TaskListComponent.scss'
 
-const TaskListComponent = () => {
+const TaskListComponent = ({ categories }) => {
   return (
     <>
 
         <h1 className='tasks-title'>Tasks for Jan 1st:</h1>
 
         <div className='tasks-list'>
-            <div className='tasks-list__category'>Networking, Career, and Events</div>
+
+          {categories.map(category => (
+
+            <div key={category.id} className={`tasks-list__category tasks-list__category--${category.color}`}>{category.name}</div>
+          ))}
+            {/* <div className='tasks-list__category'>Networking, Career, and Events</div> */}
         </div>
     
     </>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import SubheaderComponent from '../../components/SubheaderComponent/SubheaderComponent'
 import CalendarComponent from '../../components/CalendarComponent/CalendarComponent'
 import TaskListComponent from '../../components/TaskListComponent/TaskListComponent'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const HomePage = () => {
+  const baseUrl = import.meta.env.VITE_APP_BASE_URL;
+
+  const [categories, setCategories] = useState([])
+
+  async function getAllCategories() {
+    const response = await axios.get(`${baseUrl}/categories`)
+    setCategories(response.data)
+  }
+
+  useEffect( () => {
+    getAllCategories()
+  }, [])
+
+  console.log(categories)
   return (
     <>
         <SubheaderComponent/>
         <CalendarComponent />
-        <TaskListComponent />
+        <TaskListComponent categories={categories} />
     </>
   )
 }
