59e14c2010b6fe9d29fb47c01454ca661b33934a
ryanmody <rahimmody488@gmail.com>
2024-09-22 16:00:57 -0400

    Add a route page that automatically takes users to the current date and month when they first visit the website

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage/HomePage.jsx";
 import FooterComponent from "./components/FooterComponent/FooterComponent.jsx";
 import AddCategoryPage from "./pages/AddCategoryPage/AddCategoryPage.jsx";
 import AddTaskPage from "./pages/AddTaskPage/AddTaskPage.jsx";
+import RoutePage from "./pages/RoutePage/RoutePage.jsx";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
 
         <main className="main">
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<RoutePage />} />
             <Route path="/addcategory" element={<AddCategoryPage />} />
             <Route path="/addtask" element={<AddTaskPage />} />
             <Route path="/:month/:date" element={<HomePage />} />
